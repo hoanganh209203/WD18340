3c69e1c8f92357792457a18b09f4f1c1e1533948
hoanganh209203 <129678025+hoanganh209203@users.noreply.github.com>
2024-04-08 19:32:45 +0700

    update code

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Route, Routes, useNavigate } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import HomePage from './pages/HomePage'
 import About from './pages/About'
@@ -10,22 +10,22 @@ import Layout from './layouts/Layout'
 import AdminPage from './layouts/AdminPage'
 import ListProducts from './pages/Admins/ListProducts'
 import Dashboard from './pages/Admins/Dashboard'
-// import AddProduct from './pages/Admins/AddProduct'
-// import UpateProduct from './pages/Admins/UpateProduct'
+import AddProduct from './pages/Admins/AddProduct'
+import UpateProduct from './pages/Admins/UpateProduct'
 import ListCategory from './pages/Admins/ListCategory'
 import { useEffect, useState } from 'react'
 
-import AddProduct_Demon from './pages/Admins/AddProduct_Demon'
-import { FormProduct } from './types/FormProduct'
-import { createProduct, updateProduct } from './Apis/Products'
+// import AddProduct_Demon from './pages/Admins/AddProduct_Demon'
+// import { FormProduct } from './types/FormProduct'
+// import { createProduct, updateProduct } from './Apis/Products'
 import { productsCoreType } from './types/Product'
-import UpdateProduct_Demon from './pages/Admins/UpdateProduct_Demo'
+// import UpdateProduct_Demon from './pages/Admins/UpdateProduct_Demo'
 import { userType } from './types/User'
 import {onRegister } from './Apis/User'
 import axios from 'axios'
 import PrivateRouter from './PrivateRouter'
 function App() {
-  const navigate = useNavigate();
+  // const navigate = useNavigate();
   const [products, setProducts] = useState<productsCoreType[]>([])
   const [users, setUsers] = useState<userType[]>([])
   useEffect(() => {
@@ -35,22 +35,22 @@ function App() {
       })
   }, [])
 
-  const handleAddProduct = (data: FormProduct) => {
-    (async () => {
-      const newProduct = await createProduct(data);
-      setProducts([...products, newProduct]);
-      navigate("/admin/listpro");
-    })();
-  };
-  const handleEditProduct = (data: FormProduct) => {
-    (async () => {
-      const product = await updateProduct(data);
-      setProducts(
-        products.map((item) => (item._id === product._id ? product : item))
-      );
-      navigate("/admin/listpro");
-    })();
-  };
+  // const handleAddProduct = (data: FormProduct) => {
+  //   (async () => {
+  //     const newProduct = await createProduct(data);
+  //     setProducts([...products, newProduct]);
+  //     navigate("/admin/listpro");
+  //   })();
+  // };
+  // const handleEditProduct = (data: FormProduct) => {
+  //   (async () => {
+  //     const product = await updateProduct(data);
+  //     setProducts(
+  //       products.map((item) => (item._id === product._id ? product : item))
+  //     );
+  //     navigate("/admin/listpro");
+  //   })();
+  // };
   const handleRegister = (user: userType) => {
     (async () => {
       const data = await onRegister(user);
@@ -83,10 +83,10 @@ function App() {
       <Route path='admin' element={<PrivateRouter user={userLogin}><AdminPage/></PrivateRouter> }>
       <Route index element={<Dashboard/>}/>
       <Route path='listpro' element={<ListProducts products={products}/>}/>
-      <Route path='listpro/add' element={<AddProduct_Demon onAdd={handleAddProduct}/>}/>
-      <Route path="listpro/edit/:id" element={<UpdateProduct_Demon onEdit={handleEditProduct} />}/>
-      {/* <Route path='listpro/add' element={<AddProduct/>}/>
-      <Route path='listpro/edit/:id' element={<UpateProduct/>}/> */}
+      {/* <Route path='listpro/add' element={<AddProduct_Demon onAdd={handleAddProduct}/>}/>
+      <Route path="listpro/edit/:id" element={<UpdateProduct_Demon onEdit={handleEditProduct} />}/> */}
+      <Route path='listpro/add' element={<AddProduct/>}/>
+      <Route path='listpro/edit/:id' element={<UpateProduct/>}/>
       <Route path='listcate' element={<ListCategory/>}/>
       </Route>
     </Routes>
